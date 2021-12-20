262237411a07e7bcb5e8a1924443a98b73320c49
ASUS <Yehor Kholodiuk>
2021-12-20 15:01:18 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './index.css';
+
 
 function App() {
+
+  const date = '20th of December 2021';
+
+  const users = [
+    {id: 1, firstName: 'Alice', lastName: 'Smith', age: 25, city: 'Los Angeles', active: true},
+    {id: 2, firstName: 'Mary', lastName: 'Adams', age: 28, city: 'San Diego', active: true},
+    {id: 3, firstName: 'Bob', lastName: 'Brown', age: 15, city: 'New York', active: false},
+  ];
+
+  const fullUserName = (user) => {
+    return user.firstName + ' ' + user.lastName
+  }
+
+  const isUserAdult = (user) => {
+    if (user.age <= 18) {
+      return <b>'No'</b>
+    }
+    return <b>'Yes'</b>
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+        <h1>List of users information</h1>
+        {date}
+        <hr/>
+        {users.map(el =>
+            <div>
+              <h2>{fullUserName(el)}</h2>
+              <ul>
+                <li>Age: {el.age}</li>
+                <li>City: {el.city}</li>
+                <li>Is user adult: {isUserAdult(el)}</li>
+                <li style={el.active ? {} : {textDecoration: 'line-through'}}>User is active</li>
+              </ul>
+              <hr/>
+            </div>
+        )}
+      </div>
   );
 }
 
